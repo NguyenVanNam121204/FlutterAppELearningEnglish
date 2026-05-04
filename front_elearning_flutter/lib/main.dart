c4de5ae3d63fe6610b0e2347eb638f5ec558e8c9
Minhxoan24 <142246049+Minhxoan24@users.noreply.github.com>
2026-05-04 23:14:54 +0700

    update

--- front_elearning_flutter/lib/main.dart
+++ front_elearning_flutter/lib/main.dart
@@ -10,9 +10,7 @@ import 'app/theme/theme_provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GoRouter.optionURLReflectsImperativeAPIs = true;
-  if (const String.fromEnvironment('APP_ENV', defaultValue: 'dev') == 'dev') {
-    await dotenv.load(fileName: '.env');
-  }
+  await dotenv.load(fileName: '.env');
 
   final prefs = await SharedPreferences.getInstance();
 
